81615fc4a55d961d0769a57b43ee7a1538802b5c
yewero <tianf@mail.cbi.pku.edu.cn>
2018-05-06 11:47:08 +0800

    improve speed for mm_search

--- scRNA-seq/scripts/cleanFq.pl
+++ scRNA-seq/scripts/cleanFq.pl
@@ -17,13 +17,13 @@ print "('$fq1', '$fq2', '$bcdToSp', '$output_path', '$trim_left', '$trim_right')
 # 0. functions
 sub hamming_distances {
 	my ($seq1, @seqks) = @_;
+	my @seq1s = split(//, $seq1);
 	my @dsts;
 	for(my $i=0; $i<@seqks; $i++) {
 		my $seq2 = $seqks[$i];
-		my @seq1s = split(//, $seq1);
 		my @seq2s = split(//, $seq2);
 		my $dst = 0;
-		for(my $j=0; $j<@seq1s; $j++) {
+		for(my $j=0; $j<@seq1s; $j++) {	# for each base pair
 			$dst ++ if $seq1s[$j] ne $seq2s[$j];
 		}
 		push @dsts, $dst;
@@ -99,25 +99,25 @@ my $inner_len = length ( (keys %inner_dict)[0] );
 # 2. split_fastq
 sub mm_search {
 	my ($query, @cands) = @_;
-	my ($out, $dst);
-
+	my $out;
+	#my $dst;
 	my @distances = &hamming_distances($query, @cands);
 	for(my $i=0; $i<@distances; $i++) {
 		my $distance = $distances[$i];
 		if($distance <= 1) {
 			if(defined $out) {	# avoid multiple mapping
 				$out = "NA";
-				$dst = -2;
+				#$dst = -2;
 				last;
 			} else {
 				$out = $cands[$i];
-				$dst = $distance;
+				#$dst = $distance;
 			}
 		}
 	}
 	if(! defined $out) {
 		$out = "NA";
-		$dst = -1;
+		#$dst = -1;
 	}
 #print "$query\t->\t$out\t$dst\n";
 	return($out);
