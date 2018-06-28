2a7661e3c7c0a96d49cee65856a71033471a73c1
yewero <tianf@mail.cbi.pku.edu.cn>
2018-06-28 14:48:35 +0800

    use new strategy for UMI clustering

--- scRNA-seq/scripts/UMIcollapse.pl
+++ scRNA-seq/scripts/UMIcollapse.pl
@@ -9,11 +9,41 @@ if (@ARGV!=2) {
 	exit;
 }
 
+sub cluster_UMI {
+# method from http://www.arrayserver.com/wiki/index.php?title=Ngs_ReportSingleCellCounts.pdf
+	my ($tmp, $cutoff) = @_;
+	my @seqs = @{$tmp};
+#print "@seqs\n";
+	my %duplicate;
+	$duplicate{$_}++ for @seqs;
+	#map{print "$_=>$duplicate{$_}","\n"}keys %duplicate;
+
+	my @sorted_seqs = sort { $duplicate{$b} <=> $duplicate{$a} or $a cmp $b } keys %duplicate;
+	#foreach my $key (@sorted_seqs) { print "$key\t$duplicate{$key}\n"; }
+
+	for(my $i=0; $i<@sorted_seqs; $i++) {
+		next if $duplicate{$sorted_seqs[$i]} == 0;
+		for(my $j=($i+1); $j<@sorted_seqs; $j++) {
+			next if $duplicate{$sorted_seqs[$j]} == 0;
+			my $dist = ($sorted_seqs[$i] ^ $sorted_seqs[$j]) =~ tr/\0//c;
+#print "> $i ($sorted_seqs[$i]:$duplicate{$sorted_seqs[$i]})\t$j ($sorted_seqs[$j]:$duplicate{$sorted_seqs[$j]})\t$dist\n";
+			if($dist <= $cutoff) {
+				$duplicate{$sorted_seqs[$i]} += $duplicate{$sorted_seqs[$j]};
+				$duplicate{$sorted_seqs[$j]} = 0;
+			}
+		}
+	}
+	#foreach my $key (@sorted_seqs) { print "$key\t$duplicate{$key}\n"; }
+	my @outs = grep { $duplicate{$_} > 0 } (@sorted_seqs);
+	return(scalar @outs);
+}
+
+
 #ENSG00000228463.9       ACAGTGGGTGTATGAATTTG
 
-my %gene;
-my $cutoff = $ARGV[1];
+my $ctof = $ARGV[1];
 
+my %gene;
 open my $FILE , $ARGV[0];
 while(<$FILE>) {
 	chomp;
@@ -30,25 +60,9 @@ foreach my $id (keys %gene) {	# for each gene
 #print "> $id\n";
 	my @seqs = split("-", $gene{$id});
 	my @sequ = do { my %seen; grep { !$seen{$_}++ } @seqs };	# unique
-#print "$id\t" . scalar @seqs . "\t->\t" . scalar @sequ . "\n";
-	my @outs;
-	foreach my $seq (@sequ) {
-#print ">> $seq\n";
-		my $res = 1;
-		foreach my $ref (@outs) {
-#print "$seq -> $ref\n";
-			my $dist = ($seq ^ $ref) =~ tr/\0//c;
-#print "$seq\t$ref\t$dist\n";
-			if($dist <= $cutoff) {
-				$res = 0;
-				last;
-			}
-		}
-		if($res == 1) {
-			push @outs, $seq;
-		}
-	}
-#print "$id\t" . scalar @seqs . "\t->\t" . scalar @sequ . "\t->\t" . scalar @outs . "\n";
-	print "$id\t" . scalar @outs . "\n";
+	my $UMI_rmdup = &cluster_UMI(\@seqs, $ctof);
+
+#print "> $id\t" . scalar @seqs . "\t->\t" . scalar @sequ . "\t->\t" . $UMI_rmdup . "\n";
+	print "$id\t$UMI_rmdup\n";
 }
 
