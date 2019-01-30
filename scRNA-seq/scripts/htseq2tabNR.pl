fd019ff868505670f8d76a19407e2a341fe059b1
yewero <tianf@mail.cbi.pku.edu.cn>
2019-01-30 17:10:41 +0800

    add htseq2tabNR for no-mismatch

---
+++ scRNA-seq/scripts/htseq2tabNR.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use 5.010;
+
+if (@ARGV!=1) {
+	print "Usage:   perl $0 samout\n";
+	print "Example: perl $0 htout/human/PR10_XXL_501/1A_htseq.samout\n";
+	exit;
+}
+
+#CCGCATGACTTCTTGAATAA_0_5T_A00455:12:H7KTCDSXX:4:1103:1768:6183_GATATGA_GTCGCTTGGGTGTAGTGC_GTTGTT_0
+my $UMI_len_def = 20;
+my ($UMI, $gene);
+open my $FILE , $ARGV[0];
+while(<$FILE>) {
+	chomp;
+	my @lines = split "\t";
+	my $nm = (split "_", $lines[0])[7];
+	next if $nm gt 0;
+	if(/^([^_]+)_.*XF:Z:(\S+)/) {
+		$UMI = $1;
+#		print STDERR "Warning: [$.] Inconsistence UMI length\n" if length($UMI) != $UMI_len_def;
+		$gene = $2;
+		#next if $gene =~ /^__/;
+	} else {
+		print STDERR "Warning: [$.] No pattern found.\n";
+	}
+	print "$gene\t$UMI\n";
+}
+close $FILE;
+
