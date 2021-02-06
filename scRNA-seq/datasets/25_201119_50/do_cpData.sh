20736db4d7659db2be9886e4c87ddbf8a6d39398
yewero <tianf@mail.cbi.pku.edu.cn>
2021-02-06 16:21:54 +0800

    add scripts for dataset 23~26

---
+++ scRNA-seq/datasets/25_201119_50/do_cpData.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+	echo "Copy rawdata from source"
+	echo "Usage: bash $0 source_dir"
+	exit
+fi
+
+IN=$1
+OUT=00-rawdata_copy
+
+rsync -avP --delete $IN/* $OUT/
