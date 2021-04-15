c1c9cf6fb5ee53e9543b098b910339fb4a64b137
yewero <tianf@mail.cbi.pku.edu.cn>
2021-04-15 09:02:05 +0800

    use uniform ident2clgrp

--- scRNA-seq/pooled_data/All/do_poolData.r
+++ scRNA-seq/pooled_data/All/do_poolData.r
@@ -189,34 +189,7 @@ cellMeta_final_filtered <- subset(cellMeta_final, QC)
 write.table(x = cellMeta_final_filtered, file = "cell_metatable_filtered.txt", row.names = F, col.names = T, quote = F, sep = "\t")
 
 # cell group
-ident2clgrp <- function(ident.input) {
-  ident.input[grepl("^Epi", ident.input)] <- "Epithelial"
-  ident.input[grepl("^Endo", ident.input)] <- "Endothelial"
-  ident.input[grepl("^SM-", ident.input)] <- "Smooth muscle"
-  ident.input[grepl("^SM$", ident.input)] <- "Smooth muscle"
-  ident.input[grepl("^SKM$", ident.input)] <- "Skeletal muscle"
-  ident.input[grepl("^Fibro", ident.input)] <- "Fibroblast"
-  # immune
-  ident.input[grepl("^B-", ident.input)] <- "B"
-  ident.input[grepl("^Pro-B", ident.input)] <- "B"
-  ident.input[grepl("^Pre-B", ident.input)] <- "B"
-  ident.input[grepl("^DC/Macro", ident.input)] <- "DC/Macrophage"
-  ident.input[grepl("^Mast-", ident.input)] <- "Mast"
-  ident.input[grepl("^Neutrophil-", ident.input)] <- "Neutrophil"
-  ident.input[grepl("^NKT-", ident.input)] <- "NKT"
-  ident.input[grepl("^T-", ident.input)] <- "T"
-  ident.input[grepl("^Pre-T", ident.input)] <- "T"
-  #
-  ident.input[grepl("^Erythrocyte-", ident.input)] <- "Erythrocyte"
-  ident.input[grepl("^CACNA1A-", ident.input)] <- "CACNA1A"
-  ident.input[ident.input %in% c("PT", "LoH", "LoH-Prog", "DT", "PC-CLU", "PC-BCAT1", "Podocyte-GPC3", "Podocyte-PLA2R1")] <- "Epithelial"
-  ident.input[grepl("^Sertoli-", ident.input)] <- "Sertoli"
-  ident.input[grepl("^Granulosa-", ident.input)] <- "Granulosa"
-  # FGC
-  ident.input[grepl("^SSC$", ident.input)] <- "FGC"
-  return(ident.input)
-}
-
+source("../../scripts/cellType_tools.r")
 cellMeta_final_filtered_plus <- cellMeta_final_filtered
 cellMeta_final_filtered_plus$group <- ident2clgrp(cellMeta_final_filtered_plus$ident)
 cellMeta_final_filtered_plus <- cellMeta_final_filtered_plus[, c(1:8,21,9:20)]
