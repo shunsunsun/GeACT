a63f2fc0335ff057da5f4724ed142a4d16105671
yewero <tianf@mail.cbi.pku.edu.cn>
2021-04-15 11:03:33 +0800

    use uniform ident2clgrp

--- scRNA-seq/pooled_data/All/do_dendogram.r
+++ scRNA-seq/pooled_data/All/do_dendogram.r
@@ -7,6 +7,7 @@ library("Matrix")
 library("pheatmap")
 library("dendextend")
 library("parallel")
+source("../../scripts/cellType_tools.r")
 #source("../../scripts/cluster_tools.r")
 
 samplingPos <- "."
@@ -23,34 +24,6 @@ chromtainRM <- read.table("../../Data/human_cr.txt", header = F, sep = "\t", str
 dim(chromtainRM)
 cellSurface <- read.table("../../Data/human_cs.txt", header = F, sep = "\t", stringsAsFactors = F)
 dim(cellSurface)
-
-ident2clgrp <- function(ident.input) {
-  ident.input[grepl("^Epi", ident.input)] <- "Epithelial"
-  ident.input[grepl("^Endo", ident.input)] <- "Endothelial"
-  ident.input[grepl("^SM-", ident.input)] <- "Smooth muscle"
-  ident.input[grepl("^SM$", ident.input)] <- "Smooth muscle"
-  ident.input[grepl("^SKM$", ident.input)] <- "Skeletal muscle"
-  ident.input[grepl("^Fibro", ident.input)] <- "Fibroblast"
-  # immune
-  ident.input[grepl("^B-", ident.input)] <- "B"
-  ident.input[grepl("^Pro-B", ident.input)] <- "B"
-  ident.input[grepl("^Pre-B", ident.input)] <- "B"
-  ident.input[grepl("^DC/Macro", ident.input)] <- "DC/Macrophage"
-  ident.input[grepl("^Mast-", ident.input)] <- "Mast"
-  ident.input[grepl("^Neutrophil-", ident.input)] <- "Neutrophil"
-  ident.input[grepl("^NKT-", ident.input)] <- "NKT"
-  ident.input[grepl("^T-", ident.input)] <- "T"
-  ident.input[grepl("^Pre-T", ident.input)] <- "T"
-  #
-  ident.input[grepl("^Erythrocyte-", ident.input)] <- "Erythrocyte"
-  ident.input[grepl("^CACNA1A-", ident.input)] <- "CACNA1A"
-  ident.input[ident.input %in% c("PT", "LoH", "LoH-Prog", "DT", "PC-CLU", "PC-BCAT1", "Podocyte-GPC3", "Podocyte-PLA2R1")] <- "Epithelial"
-  ident.input[grepl("^Sertoli-", ident.input)] <- "Sertoli"
-  ident.input[grepl("^Granulosa-", ident.input)] <- "Granulosa"
-  # FGC
-  ident.input[grepl("^SSC$", ident.input)] <- "FGC"
-  return(ident.input)
-}
 
 clgrp2color <- function(clgrp.input, color.input = c("tomato", "orange", "#1ADBB8", "#00C9C9", "#00BF74", 
                                                      "#8258FA", "#D358F7", "#D0A9F5", "dodgerblue2", "firebrick3")) {
