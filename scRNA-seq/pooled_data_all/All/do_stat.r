f465cac0c53c197d63c0ac7df917bd34660bf124
yewero <tianf@mail.cbi.pku.edu.cn>
2021-05-19 16:58:58 +0800

    more detail stat

--- scRNA-seq/pooled_data_all/All/do_stat.r
+++ scRNA-seq/pooled_data_all/All/do_stat.r
@@ -52,6 +52,65 @@ dev.off()
 #   geom_text(aes(label = value, y = value), size = 3, position = position_stack(vjust = 0.5), show.legend = F) + 
 #   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+# stat by cell group
+load("../../pooled_data/All/03-expression/merged/cellCluster/cg_color.RData")
+group_shown <- c("Epithelial", "Endothelial", "Smooth muscle", "Fibroblast", "B", 
+                 "DC/Macrophage", "NKT", "T", "Glial", "FGC", 
+                 "Granulosa", "Sertoli", "Erythrocyte", "Other")
+
+cellMetaData <- read.table(file = "cell_metatable_RNA_global.txt", header = T, sep = "\t", stringsAsFactors = F)
+cellMetaData$group_new <- cellMetaData$group
+cellMetaData$group_new[! cellMetaData$group_new %in% group_shown] <- "Other"
+cellMetaData$group_new <- factor(cellMetaData$group_new, levels = group_shown)
+
+cellMeta_tb <- as.data.frame(table(cellMetaData$group_new))
+colnames(cellMeta_tb) <- c("group", "cellNum")
+
+pdf("cellGroup_stat.pdf", width = 4, height = 4)
+
+ggplot(cellMeta_tb, aes(x = group, y = log10(cellNum), fill = group)) + geom_bar(stat = "identity", alpha = 0.6, show.legend = F) + coord_flip() + 
+  scale_x_discrete(limits = rev(levels(cellMeta_tb$group))) + 
+  scale_y_continuous(limits = c(0, max(log10(cellMeta_tb$cellNum)) * 1.05), expand = c(0, 0), 
+                     breaks = 0:4, labels = parse(text = paste0(10, "^", 0:4))) + 
+  scale_fill_manual(values = cg_color[levels(cellMeta_tb$group)]) + 
+  xlab("Cell group") + ylab("Cell number") + 
+  theme(plot.subtitle = element_text(hjust = 0.5))
+
+dev.off()
+#
+
+# stat by week
+load("../../pooled_data/All/03-expression/merged/cellCluster/ct_color.RData")
+
+cellMeta <- read.table(file = "cell_metatable_RNA_global.txt", header = T, sep = "\t", stringsAsFactors = F)
+cellMeta$tissue <- Hmisc::capitalize(cellMeta$tissue)
+cellMeta$tissue <- factor(cellMeta$tissue, levels = rev(names(ct_color)))
+
+plateMeta <- read.table(file = "TableS1-RNA_final.txt", header = T, sep = "\t", stringsAsFactors = F)
+plateMeta$tissue <- Hmisc::capitalize(plateMeta$tissue)
+plateMeta$tissue <- factor(plateMeta$tissue, levels = rev(names(ct_color)))
+
+# use the cells before filtering
+# RNA_stat <- as.data.frame(table(plateMeta[, c("donor", "week_age", "tissue")]))
+# colnames(RNA_stat)[4] <- "plate_num"
+# RNA_stat$cell_num <- 96 * RNA_stat$plate_num
+# RNA_stat <- subset(RNA_stat, cell_num > 0)
+# 
+
+cellMeta_plus <- merge(cellMeta, plateMeta[, -5], by = "plate", sort = F)
+RNA_stat <- as.data.frame(table(cellMeta_plus[, c("donor", "week_age", "tissue")]))
+RNA_stat$week_age <- as.numeric(as.character(RNA_stat$week_age))
+colnames(RNA_stat)[4] <- "cell_num"
+RNA_stat <- subset(RNA_stat, cell_num > 0)
+
+pdf("week_stat.pdf", width = 5, height = 4)
+
+ggplot(RNA_stat, aes(x = week_age, y = tissue, color = donor)) + geom_jitter(aes(size = cell_num), width = 0, height = 0) + 
+  labs(color = "Donor", size = "Cell number") + xlab("Age (week)") + ylab("Organ") + 
+  scale_x_continuous(breaks = seq(11, 22, 3))
+
+dev.off()
+
 pdf("expr_stat.pdf", width = 4, height = 4)
 
 # ggplot(cellMeta_ftd, aes(x = tissue, y = nGene, fill = tissue)) + geom_violin(show.legend = F) + coord_flip() + 
@@ -88,6 +147,11 @@ dev.off()
 ### ATAC-seq ----
 cellMeta <- read.table("cell_metatable_ATAC.txt", header = T, sep = "\t", stringsAsFactors = F)
 dim(cellMeta)
+cat("Effective plate number:", length(unique(cellMeta$plate)), "\n")
+cat("Effective cell number (before filtering):", 96 * length(unique(cellMeta$plate)), "\n")
+cat("Position number:", nrow(unique(cellMeta[, c("tissue", "samplingPos")])), "\n")
+cat("Ident number:", nrow(unique(subset(cellMeta, ident != "Unknown", c("tissue", "ident")))), "\n")
+
 cellMeta <- cellMeta[, c("tissue", "cleanReads", "nFrags", "nPeak")]
 colnames(cellMeta) <- c("tissue", "cleanReads", "frag_num", "peak_num")
 cellMeta$tissue <- gsub("_", " ", Hmisc::capitalize(cellMeta$tissue))
